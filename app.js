815a8e63b19154bf73590b666458e632425c01f9
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-06-12 16:00:47 -0400

    Move Quickstart pages under "/quickstart/"

--- app.js
+++ app.js
@@ -51,6 +51,12 @@ app.use(express.static(config.dirs.pub));
 // -- Routes -------------------------------------------------------------------
 
 router.route('/').get(routes.render('home'));
-router.route('/browser/').get(routes.render('browser'));
-router.route('/node/').get(routes.render('node'));
+
+router.route('/quickstart/')
+    .get(routes.render('quickstart'));
+router.route('/quickstart/browser/')
+    .get(routes.render('quickstart/browser'));
+router.route('/quickstart/node/')
+    .get(routes.render('quickstart/node'));
+
 router.route('/handlebars/').get(routes.handlebars);

3515443fdfe555e790962b0b84ed75fe7be64caa
Clarence Leung <clarle@yahoo-inc.com>
2014-06-17 10:17:19 -0700

    Wrap everything in a IFEE

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,16 @@
 //Client-side JS goes here.
-var locale    = APP.intl.locale,
+(function () {
+    var locale    = APP.intl.locale,
     baseUrl   = 'http://yui.yahooapis.com/combo?platform/intl/0.1.2/Intl.min.js&platform/intl/0.1.2/locale-data/jsonp/',
     comboUrl  = baseUrl + locale + '.js';
 
-yepnope({
-    test : !!window.Intl,
-    nope : comboUrl
-    callback: init
-});
+    yepnope({
+        test : !!window.Intl,
+        nope : comboUrl
+        callback: init
+    });
 
-function init () {
-    // Initialize application here
-}
+    function init () {
+        // Initialize application here
+    }
+})();

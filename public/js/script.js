d34142022ccee2c72cf99430bba1ad3446061294
Clarence Leung <clarle@yahoo-inc.com>
2014-06-17 11:53:47 -0700

    Load in `intl-messageformat` after Intl

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,17 @@
 //Client-side JS goes here.
 (function () {
     var locale    = APP.intl.locale,
-    baseUrl   = 'http://yui.yahooapis.com/combo?platform/intl/0.1.2/Intl.min.js&platform/intl/0.1.2/locale-data/jsonp/',
-    comboUrl  = baseUrl + locale + '.js';
+        // TODO: expose these URLs through Express State instead
+        baseUrl   = 'http://yui.yahooapis.com/combo?platform/intl/0.1.2/Intl.min.js&platform/intl/0.1.2/locale-data/jsonp/',
+        comboUrl  = baseUrl + locale + '.js',
+        intlMessageFormatUrl = '/node_modules/intl-messageformat/build/intl-messageformat.complete.min.js';
 
-    yepnope({
+    yepnope([{
         test : !!window.Intl,
         nope : comboUrl
-        callback: init
-    });
+    }, intlMessageFormatUrl, {
+        complete: init
+    }]);
 
     function init () {
         // Initialize application here

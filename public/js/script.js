4dde0e7e560aeedad2d4f0b50954b2c7ece89485
Clarence Leung <clarle@yahoo-inc.com>
2014-06-17 09:55:25 -0700

    Dynamically load Intl.js + locale data

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
 //Client-side JS goes here.
+var locale    = APP.intl.locale,
+    baseUrl   = 'http://yui.yahooapis.com/combo?platform/intl/0.1.2/Intl.min.js&platform/intl/0.1.2/locale-data/jsonp/',
+    comboUrl  = baseUrl + locale + '.js';
+
+yepnope({
+    test : !!window.Intl,
+    nope : comboUrl
+    callback: init
+});
+
+function init () {
+    // Initialize application here
+}

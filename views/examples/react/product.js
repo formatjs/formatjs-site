480dd4740a6b1e9b3c6ab3f79e23e090e8d1e4a0
Tilo Mitra <tilomitra@gmail.com>
2014-06-24 11:40:33 -0400

    initial commit of examples

---
+++ views/examples/react/product.js
@@ -0,0 +1,22 @@
+var React = require('react'),
+    ReactIntlMixin = require('react-intl');
+
+var MyComponent = React.createClass({
+    mixins: [ReactIntlMixin],
+    getMyMessage: function () {
+        return "{product} will cost {price, number} if ordered by {deadline, date}"
+    },
+    render: function () {
+        return React.DOM.div(null,
+            React.DOM.p(null, this.intlDate(new Date())),
+            React.DOM.p(null, this.intlNumber(600)),
+            React.DOM.p(null, this.intlMessage(this.getMyMessage(), {
+                product: 'Mac Mini',
+                price: 2000.0015,
+                deadline: 1390518044403
+            }))
+        );
+    }
+});
+
+module.exports = MyComponent;

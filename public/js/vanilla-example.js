28f4b80b56db480c39031539a2187d69c8d12cae
Andrew Wooldridge <triptych@gmail.com>
2014-06-25 13:56:41 -0700

    new-hp-example

--- public/js/vanilla-example.js
+++ public/js/vanilla-example.js
@@ -18,7 +18,6 @@ var VanillaExample = {
     },
     now: new Date(),
     init: function(){
-        //alert("hi");
         this.setup();
         this.bind();
     },
@@ -28,33 +27,31 @@ var VanillaExample = {
             //console.log(switcher);
         for(var key in trans){
             if(trans.hasOwnProperty(key)){
-                console.log(key + " -> " + trans[key]);
+                //console.log(key + " -> " + trans[key]);
                 var opt = document.createElement("option");
                 opt.setAttribute("value", key);
                 opt.textContent = key;
-                console.log(opt);
                 switcher.appendChild(opt);
             }
         }
         this.render(this);
     },
     render: function(obj){
-        console.log(obj);
-        var chosenLocale = document.querySelectorAll(".switcher select")[0].value;
-        var msg = new IntlMessageFormat(APP.translations[chosenLocale].USER_HAS_BOOKS,chosenLocale);
-        var msg2 = new IntlMessageFormat(APP.translations[chosenLocale].USER_WILL_SELL,chosenLocale, this.intlFormat);
-        var out = msg.format({
-            firstName: obj.user.firstName,
-            lastName: obj.user.lastName,
-            numBooks: obj.user.numBooks
-        });
-        var out2 = msg2.format({
-            firstName: obj.user.firstName,
-            numBooks: 10,
-            price: 1000,
-            dateBooks: this.now
-        });
-        //console.log(out);
+        var chosenLocale = document.querySelectorAll(".switcher select")[0].value,
+            msg = new IntlMessageFormat(APP.translations[chosenLocale].USER_HAS_BOOKS,chosenLocale),
+            msg2 = new IntlMessageFormat(APP.translations[chosenLocale].USER_WILL_SELL,chosenLocale, this.intlFormat),
+            out = msg.format({
+                firstName: obj.user.firstName,
+                lastName: obj.user.lastName,
+                numBooks: obj.user.numBooks
+            }),
+            out2 = msg2.format({
+                firstName: obj.user.firstName,
+                numBooks: 10,
+                price: 1000,
+                dateBooks: this.now
+            });
+
         document.getElementsByClassName("message")[0].innerText = out + out2;
 
     },

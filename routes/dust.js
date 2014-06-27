b5ec6e859d45c2b68bcca4a43e397707f5af68a4
Tilo Mitra <tilomitra@gmail.com>
2014-06-27 17:33:37 -0400

    separate context object, dont cache example compilation for now

--- routes/dust.js
+++ routes/dust.js
@@ -8,23 +8,27 @@ var getExamples = require('../lib/examples'),
 require('dust-helper-intl').register(Dust);
 
 module.exports = function (req, res, next) {
-    getExamples('dust', {cache: true}).then(function (examples) {
+    getExamples('dust', {cache: false}).then(function (examples) {
         var dustExamples = examples;
 
         Object.keys(dustExamples).forEach(function(key) {
-            var example = dustExamples[key];
+            var example = dustExamples[key],
+                context = {
+                    intl: res.locals.intl,
+                    user: {
+                        firstName: 'Tilo',
+                        lastName: 'Mitra',
+                        numBooks: '20'
+                    },
+                    dateBooks: 'Fri Jun 27 2014 13:08:19 GMT-0400'
+            };
+
             Dust.loadSource(example.compiled);
-            Dust.render(example.identifier, {
-                intl: res.locals.intl,
-                user: {
-                    firstName: 'Tilo',
-                    lastName: 'Mitra',
-                    numBooks: 20
-                }
-            }, function (err, html) {
+            Dust.render(example.identifier, context, function (err, html) {
                 if (err) {
                     Utils.error(500, err);
                 }
+
                 example.rendered = html;
 
                 //expose just the source file for each example here.

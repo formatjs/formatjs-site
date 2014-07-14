2323d15d18ff1832d517c9d45fc3bcad6e4f4910
Tilo Mitra <tilomitra@gmail.com>
2014-07-14 13:43:26 -0400

    add route for quickstart handlebars

---
+++ routes/quickstartHandlebars.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var getExamples = require('../lib/examples');
+
+module.exports = function (req, res, next) {
+    getExamples('handlebars', {cache: true}).then(function (examples) {
+        var hbsExamples = examples;
+        Object.keys(hbsExamples).forEach(function(key) {
+            var example = examples[key];
+
+            console.log(example);
+            example.rendered = example.compiled({
+                intl: res.locals.intl,
+                user: {
+                    firstName: 'Tilo',
+                    lastName: 'Mitra',
+                    travelDate: new Date(),
+                    price: 465,
+                    numBooks: 20
+                },
+                now: new Date()
+            });
+
+            //expose just the source file for each example here.
+            res.expose(example.source, 'examples.hbs.' + example.name);
+        });
+
+        //Expose this string so the necessary client-side JS file gets loaded
+        res.expose('Travel', 'example');
+        res.expose([
+            '/bower_components/intl-messageformat/build/intl-messageformat.complete.min.js',
+            '/bower_components/handlebars-helper-intl/dist/helpers.js'
+        ], 'scripts');
+
+        res.render('quickstart/handlebars', {
+            examples: hbsExamples
+        });
+    }).catch(next);
+};

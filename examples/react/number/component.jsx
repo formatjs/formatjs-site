931750b2f11edecd3cb1c277c7bc7754f4d4cec9
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-09-07 15:17:40 -0400

    Add React formatNumber example

---
+++ examples/react/number/component.jsx
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+var Component = React.createClass({
+    mixins: [ReactIntlMixin],
+
+    render: function () {
+        return (
+            <ul>
+                <li>{this.formatNumber(42000)}</li>
+                <li>{this.formatNumber(0.9, {style: 'percent'})}</li>
+                <li>
+                    {this.formatNumber(100.95, {
+                        style   : 'currency',
+                        currency: 'USD'
+                    })}
+                </li>
+            </ul>
+        );
+    }
+});
--- routes/react.js
+++ routes/react.js
@@ -1,9 +1,19 @@
 'use strict';
 
+var getExamples    = require('../lib/examples').get,
+    renderExamples = require('../lib/examples').render;
+
 module.exports = function (route) {
     route.name = 'react';
 
     route.get(function (req, res, next) {
-        res.render('react');
+        getExamples('react', {
+            cache: req.app.get('view cache')
+        }).then(function (examples) {
+            res.locals.examples = renderExamples(examples, res.intl);
+            res.expose(examples, 'examples');
+            res.expose('integration', 'pageType');
+            res.render('react');
+        });
     });
 };

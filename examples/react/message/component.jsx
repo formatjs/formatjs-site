b9c468f2c5b251d51f1cb133d1a9846a7be8c0d4
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-09-07 15:27:41 -0400

    Add React formatMessage example

---
+++ examples/react/message/component.jsx
@@ -0,0 +1,18 @@
+/** @jsx React.DOM */
+var Component = React.createClass({
+    mixins: [ReactIntlMixin],
+
+    render: function () {
+        var message = this.getIntlMessage('photos');
+
+        return (
+            <p>
+                {this.formatMessage(message, {
+                    name     : 'Annie',
+                    numPhotos: 1000,
+                    takenDate: Date.now()
+                })}
+            </p>
+        );
+    }
+});

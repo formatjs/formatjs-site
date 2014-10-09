6095f0f65706a471241d5ddf43c534cc426e6517
Caridy Patino <caridy@gmail.com>
2014-10-09 19:31:55 -0400

    propagating react changes into hbs

--- examples/react/numberCustom/component.jsx
+++ examples/react/numberCustom/component.jsx
@@ -7,7 +7,7 @@ var Component = React.createClass({
             <ul>
                 <li>{this.formatNumber(42000)}</li>
                 <li>{this.formatNumber(0.9,    'percentage')}</li>
-                <li>{this.formatNumber(100.95, 'coinage')}</li>
+                <li>{this.formatNumber(100.95, 'USD')}</li>
             </ul>
         );
     }

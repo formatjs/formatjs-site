856281e9e3c4663ac17e394ab7259981ea0812bd
Eric Ferraiuolo <edf@ericf.me>
2015-03-24 18:53:04 -0400

    Switch to ignore patterns for istanbul hook

--- tests/istanbul-hook.js
+++ tests/istanbul-hook.js
@@ -1,24 +1,25 @@
 'use strict';
 
-var path     = require('path');
-var istanbul = require('istanbul');
+var istanbul  = require('istanbul');
+var minimatch = require('minimatch');
 
 // Set of files we want Istanbul to _really_ ignore.
 var IGNORE_FILES = [
-    './node_modules/es6-shim/es6-shim.js'
+    '**/node_modules/**'
 ];
+
+function isIgnoredFile(file) {
+    return IGNORE_FILES.some(function (pattern) {
+        return minimatch(file, pattern);
+    });
+}
 
 // Override Istanbul's `require()` hook, so that certain modules, like
 // polyfills, can be _really_ ignored from Istanbul coverage numbers.
 var istanbulRequireHook = istanbul.hook.hookRequire;
 istanbul.hook.hookRequire = function (matcher, transformer, options) {
-    var ignoreFiles = IGNORE_FILES.reduce(function (hash, filename) {
-        hash[path.resolve(filename)] = true;
-        return hash;
-    }, {});
-
     istanbulRequireHook(matcher, function (code, filename) {
-        if (ignoreFiles[filename]) {
+        if (isIgnoredFile(filename)) {
             console.log('Ignoring from code coverage: %s', filename);
             return code;
         }
